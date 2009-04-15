5b041872aa92395579bb86576f27a4e51496381d
Adam Greig <random@randomskk.net>
2009-04-15 02:31:12 +0100

    COMMAND_INDICATOR is now a list and added comments to settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# The IRC username and password to connect and identify with
 USERNAME = 'momobot2'
 PASSWORD = 'momo'
 
+# The IRC server and port to connect to
 SERVER = 'irc.rizon.net'
 PORT = 6667
 
+# The channel to join
 CHANNEL = '#momobot'
 
-COMMAND_INDICATOR = '!'
+# A list of command indicators
+COMMAND_INDICATOR = ['!', '.', 'momo, ']
