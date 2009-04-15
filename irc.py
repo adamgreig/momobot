fca0203e5a9b07b8a5108b6d62fe1c37ca091a19
Adam Greig <random@randomskk.net>
2009-04-15 01:38:06 +0100

    Added documentation and fixed up the docstrings

--- irc.py
+++ irc.py
@@ -11,9 +11,9 @@ class IRC:
     def __init__(self, server, port, nickname):
         """
         Connect to the server and set our nickname
-        @param server The IRC server to connect to
-        @param port The IRC port to connect to
-        @param nickname The nickname to use on IRC
+        server: The IRC server to connect to
+        port: The IRC port to connect to
+        nickname: The nickname to use on IRC
         """
         self.nickname = nickname
         self.callbacks = {}
@@ -26,14 +26,14 @@ class IRC:
     def identify(self, password):
         """
         Identify with NickServ
-        @param password The NickServ password
+        password: The NickServ password
         """
         self.socket.send('PRIVMSG nickserv :identify %s\r\n' % password)
     
     def join(self, channel):
         """
         Join a channel
-        @param channel The channel to join, including the #
+        channel: The channel to join, including the #
         """
         self.channel = channel
         self.socket.send('JOIN %s\r\n' % channel)
@@ -47,7 +47,7 @@ class IRC:
     def quit(self, message='Momobot Disconnected'):
         """
         Quit the server
-        @param message The quit message to use
+        message: The quit message to use
         """
         self.socket.send('QUIT :%s\r\n' % message)
         self.socket.close()
@@ -55,23 +55,23 @@ class IRC:
     def say(self, message):
         """
         Say a message to the channel
-        @param message Message to say
+        message: Message to say
         """
         self.socket.send('PRIVMSG %s :%s\r\n' % (self.channel, message))
     
     def privmsg(self, user, message):
         """
         PM someone
-        @param user The username to PM
-        @param message The message to send them
+        user: The username to PM
+        message: The message to send them
         """
         self.socket.send('PRIVMSG %s :%s\r\n' % (user, message))
     
     def notice(self, user, message):
         """
         Send someone a notice
-        @param user The username to send the notice to
-        @param message The message to send them
+        user: The username to send the notice to
+        message: The message to send them
         """
         self.socket.send('NOTICE %s :%s\r\n' % (user, message))
     
@@ -80,7 +80,7 @@ class IRC:
         Read in data from the IRC socket then parse it
         """
         self.buffer = self.socket.recv(4096)
-        self.parse()
+        self.__parse()
     
     def __parse(self):
         """
@@ -97,7 +97,7 @@ class IRC:
             except IndexError:
                 pass
             else:
-                self.process_irc_command(sender, command, text)
+                self.__process_irc_command(sender, command, text)
             finally:
                 self.buffer = self.buffer[self.buffer.find('\r\n') + 2:]
     
@@ -112,9 +112,9 @@ class IRC:
         
         PRIVMSG: the main command, split it up and see what it is
         
-        @param sender The full sender string
-        @param command The command from the IRC server
-        @param text Everything following the command
+        sender: The full sender string
+        command: The command from the IRC server
+        text: Everything following the command
         """
         if command == "PING":
             self.socket.send('PONG %s\r\n' % text)
@@ -130,7 +130,7 @@ class IRC:
                 return
             
             if target == self.channel:
-                self.callback('channel_message', {'username': username, 'message': message})
+                self.__callback('channel_message', {'username': username, 'message': message})
             elif message == "\001VERSION\001":
                 self.socket.send('NOTICE %s :\001VERSION Momobot v0.1a1\001\r\n' % username)
             elif message.find("\001PING") == 0:
@@ -141,8 +141,8 @@ class IRC:
     def __callback(self, callback_type, data):
         """
         Try to call a registered callback with the proper data dictionary
-        @param callback_type The callback to try calling. E.g., 'channel_message'
-        @param data The dictionary that should be passed to the callback function
+        callback_type: The callback to try calling. E.g., 'channel_message'
+        data: The dictionary that should be passed to the callback function
         """
         try:
             callback = self.callbacks[callback_type]
@@ -153,8 +153,8 @@ class IRC:
     def register_callback(self, callback_type, callback):
         """
         Register a callback function
-        @param callback_type The type of callback, e.g., 'channel_message'
-        @param callback The actual function that should be called. It must
+        callback_type: The type of callback, e.g., 'channel_message'
+        callback: The actual function that should be called. It must
         accept a single argument, a dictionary of the relevent data.
         """
         self.callbacks[callback_type] = callback
