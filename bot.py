9bf587dfdd30536664b36bd2df3e0a5bf8096d61
Adam Greig <random@randomskk.net>
2009-06-25 22:06:14 +0100

    Fixed message_queue code and moved it to its own file.

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import socket
 import time
 import irc
 import command_loader
-import datetime
-import Queue
 import sys
+
+import message_queue
 
 class BotError(Exception):
     pass
@@ -17,67 +17,8 @@ class BotError(Exception):
 class CommandError(BotError):
     def __str__(self):
         return "The command being registered was not callable."
-    
-class Message:
-    def __init__(self, type, message, target):
-        self.type = type
-        self.message = message
-        self.target = target            
-    
-class MessageQueue:
-    # Message Types
-    __sayMsg = 'say'
-    __actMsg = 'act'
-    __privMsg = 'priv'
-    __noticeMsg = 'notice'
-    
-    __irc = None
-    __timeMessageLastSent = None
-    __messageWaitMilliseconds = None
-    
-    
-    def __init__(self, irc, messagesPerSecond):
-        if messagesPerSecond <= 0:
-            raise Exception, "messagesPerSecond must be greater than 0."
-    
-        self.__messageWaitMilliseconds = datetime.timedelta(milliseconds=(1000 / messagesPerSecond))
-        self.__queue = Queue.LifoQueue()
-    
-    def addSayMessage(self, message, channel=''):
-        self.__queue.put(message(__sayMsg, message, channel))
-    
-    def addActMessage(self, message, channel=''):
-        self.__queue.put(message(__actMsg, message, channel))
-    
-    def addPrivateMessage(self, user, message):
-        self.__queue.put(message(__privMsg, message, user))
-    
-    def addNoticeMessage(self, user, message):
-        self.__queue.put(message(__noticeMsg, message, user))
-        
-    def process_message_queue(self):
-        if self.__queue.qsize() == 0:
-            return
-
-        if self.__timeMessageLastSent != None and (self.__timeMessageLastSent - datetime.now) <= self.__messageWaitMilliseconds:
-            return
-
-        currentMessage = self.__queue.get()
-        
-        if currentMessage.type == __sayMsg:
-            self.__irc.say(currentMessage.message, currentMessage.target)
-        elif currentMessage.type == __actMsg:
-            self.__irc.act(currentMessage.message, currentMessage.target)
-        elif currentMessage.type == __privMsg:
-            self.__irc.privmsg(currentMessage.target, currentMessage.message)
-        elif currentMessage.type == __noticeMsg:
-            self.__irc.notice(currentMessage.target, currentMessage.message)
-            
-        self.__timeMessageLastSent = datetime.now    
-
 
 class Bot:
-    __messageQueue = MessageQueue(irc, 2)
 
     def __init__(self, settings):
         self.settings = settings
@@ -85,6 +26,7 @@ class Bot:
         self.irc = irc.IRC(settings.NICKNAME, settings.CTCP_VERSION)
         self.commands = {}
         self.channels = []
+        self.__queue = message_queue.MessageQueue(self.irc, 2)
         command_loader.CommandLoader(self)
         self.irc.register_callback('channel_message', self.process_message)
         self.irc.connect(settings.SERVER, settings.PORT)
@@ -96,13 +38,10 @@ class Bot:
         while True:
             try:
                 self.irc.read()
-                self.__messageQueue.process_message_queue()
-            except:
-                self.irc.say('Exception occured.')
-		time.sleep(10)
-		self.irc.quit()
-                raise
-            
+                self.__queue.process_message_queue()
+            except Exception as inst:
+                self.irc.say('Exception occured: %s' % inst)
+                raise inst
     
     def process_message(self, data):
         for command_indicator in self.settings.COMMAND_INDICATORS:
@@ -119,7 +58,6 @@ class Bot:
                         command(data)
                         return
             else:
-                #irc.say(data['message'], data['channel'])
                 pass
     
     def register_command(self, command_name, command):
@@ -129,14 +67,14 @@ class Bot:
             raise CommandError
 
     def say(self, message, channel=''):
-        self.irc.say(message, channel)
+        self.__queue.addSayMessage(message, channel)
     
     def act(self, message, channel=''):
-        self.irc.act(message, channel)
+        self.__queue.addActMessage(message, channel)
     
     def privmsg(self, user, message):
-        self.irc.privmsg(user, message)
+        self.__queue.addPrivateMessage(message, channel)
     
     def notice(self, user, message):
-        self.irc.notice(user, message)
+        self.__queue.addNoticeMessage(message, channel)
         
---
+++ message_queue.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import Queue
+import datetime
+
+class Message:
+    def __init__(self, type, message, target):
+        self.type = type
+        self.message = message
+        self.target = target           
+    
+class MessageQueue:
+    
+    def __init__(self, irc, messagesPerSecond):
+        # Message Types
+        self.__sayMsg = 'say'
+        self.__actMsg = 'act'
+        self.__privMsg = 'priv'
+        self.__noticeMsg = 'notice'
+        
+        self.__irc = irc
+        self.__timeMessageLastSent = None
+        self.__messageWaitMilliseconds = None
+        
+        if messagesPerSecond <= 0:
+            raise Exception, "messagesPerSecond must be greater than 0."
+        
+        self.__messageWaitMilliseconds = datetime.timedelta(milliseconds=(1000 / messagesPerSecond))
+        self.__queue = Queue.Queue()
+    
+    def addSayMessage(self, message, channel=''):
+        self.__queue.put(Message(self.__sayMsg, message, channel))
+    
+    def addActMessage(self, message, channel=''):
+        self.__queue.put(Message(self.__actMsg, message, channel))
+    
+    def addPrivateMessage(self, user, message):
+        self.__queue.put(Message(self.__privMsg, message, user))
+    
+    def addNoticeMessage(self, user, message):
+        self.__queue.put(Message(self.__noticeMsg, message, user))
+        
+    def process_message_queue(self):
+        if self.__queue.empty():
+            return
+        
+        if self.__timeMessageLastSent != None and (datetime.datetime.now() - self.__timeMessageLastSent) <= self.__messageWaitMilliseconds:
+            return
+        
+        currentMessage = self.__queue.get()
+        
+        if currentMessage.type == self.__sayMsg:
+            self.__irc.say(currentMessage.message, currentMessage.target)
+        elif currentMessage.type == self.__actMsg:
+            self.__irc.act(currentMessage.message, currentMessage.target)
+        elif currentMessage.type == self.__privMsg:
+            self.__irc.privmsg(currentMessage.target, currentMessage.message)
+        elif currentMessage.type == self.__noticeMsg:
+            self.__irc.notice(currentMessage.target, currentMessage.message)
+            
+        self.__timeMessageLastSent = datetime.datetime.now()
